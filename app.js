2423f292ad6b8a3acabe17983430abcf61077b45
Abdullah <drivnder123@gmail.com>
2022-08-09 21:13:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// https://calculator.swiftutors.com/torque-calculator.html
+
 const v1 =  document.getElementById('v1');
 const v2 = document.getElementById('v2');
 const btn = document.getElementById('btn');
